101d316defb2d64a96eaa561904fb365f5741f0f
carleoner <kubaw4000@gmail.com>
2022-08-25 13:57:00 +0200

    lesson 3 firebase

---
+++ Vuex-4-Firebase-Auth-lesson-1/my-blog/src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX3TZfeyLaPv8P6XbvTB-vSytjQtNXGw4",
+  authDomain: "vuex-4-fb-auth-1fd5d.firebaseapp.com",
+  projectId: "vuex-4-fb-auth-1fd5d",
+  storageBucket: "vuex-4-fb-auth-1fd5d.appspot.com",
+  messagingSenderId: "21550499759",
+  appId: "1:21550499759:web:800bd6721b4480484ed15a",
+};
+
+//init firebase
+initializeApp(firebaseConfig);
+
+//init firebase auth
+const auth = getAuth();
+
+export { auth };
